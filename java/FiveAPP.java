6352bcd49ed9531f69f6fa85fbe1c6350fb4d9a6
Anton-2002 <78258802+Anton-2002@users.noreply.github.com>
2021-02-08 19:27:15 +0300

    Delete java directory

--- java/FiveAPP.java
+++
@@ -1,9 +0,0 @@
-public class FiveAPP {
-        public static void main(String[] args) {
-            int year = 2000;
-            if (!(year % 4 == 0) || ((year % 100 == 0) && !(year % 400 == 0)))
-                System.out.println(year + " г. не високосный");
-            else System.out.println(year + " г. високосный");
-        }
-
-    }
